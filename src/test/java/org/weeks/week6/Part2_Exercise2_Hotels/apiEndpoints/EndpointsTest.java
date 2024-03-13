21a5642cafb1ca4794a235214e011176b11220a5
TRossen89 <110735176+TRossen89@users.noreply.github.com>
2024-03-13 18:26:31 +0100

    week 6, testing endpoints, "/hotels/{id}" and POST hotels tested

--- src/test/java/org/weeks/week6/Part2_Exercise2_Hotels/apiEndpoints/EndpointsTest.java
+++ src/test/java/org/weeks/week6/Part2_Exercise2_Hotels/apiEndpoints/EndpointsTest.java
@@ -28,22 +28,18 @@ class EndpointsTest {
 
         IDAO hotelIDAO = new HotelDAO(emfTest);
 
+        // Creating 2 hotels with rooms
         Hotel hotel = new Hotel("Palads", "Some address 1");
-
         Room room = new Room(10, 1800.00);
         hotel.addRoom(room);
 
         hotelIDAO.create(hotel);
 
-
         Room roomTest = new Room(11, 1200.00);
-
         Hotel hotelTest = new Hotel("Test", "Tester street 1");
         hotelTest.addRoom(roomTest);
 
         hotelIDAO.create(hotelTest);
-
-        System.out.println(hotelIDAO.getById(1l));
 
     }
     @BeforeEach
@@ -64,7 +60,7 @@ class EndpointsTest {
     }
 
     @Test
-    void hotels() {
+    void allHotels() {
 
         given()
                 .when()
@@ -78,6 +74,46 @@ class EndpointsTest {
                 .assertThat()
                 .body("name", hasItem("Test"));
 
+
+    }
+
+    @Test
+    void hotelById(){
+
+        given()
+                .when()
+                .get("http://localhost:7007/api/hotels/2")
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .body("size()", greaterThan(0))
+                .assertThat()
+                .body("id", is(2))
+                .assertThat()
+                .body("name", equalTo("Test"))
+                .assertThat()
+                .body("address", is("Tester street 1"));
+
+    }
+
+    @Test
+    void createHotel(){
+        with()
+                .body(new Hotel("Hotel 5", "Hotel Street 5"))
+                .when()
+                .request("POST", "http://localhost:7007/api/hotels")
+                .then()
+                .statusCode(201);
+
+        given()
+                .when()
+                .get("http://localhost:7007/api/hotels/3")
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .body("size()", greaterThan(0))
+                .assertThat()
+                .body("id", is(3));
 
     }
 
